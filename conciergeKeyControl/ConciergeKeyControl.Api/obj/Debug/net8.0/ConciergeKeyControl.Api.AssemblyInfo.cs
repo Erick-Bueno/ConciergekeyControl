91dcd280e4c327f7f7847dfd55819dd8ef23abae
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 12:45:17 -0300

    chore: add files to .gitignore

--- conciergeKeyControl/ConciergeKeyControl.Api/obj/Debug/net8.0/ConciergeKeyControl.Api.AssemblyInfo.cs
+++ conciergeKeyControl/ConciergeKeyControl.Api/obj/Debug/net8.0/ConciergeKeyControl.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConciergeKeyControl.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d39bc7ce298e0ea5609626b462ff45de3a86176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa184211ed42e2e9c20eb1fb224ed458ffbf645")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConciergeKeyControl.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConciergeKeyControl.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
