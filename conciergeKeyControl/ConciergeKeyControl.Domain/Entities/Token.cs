b1773014332dc5ce05d8a720b68e068791f0ea8b
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 13:27:26 -0300

    feat: create the token entity

---
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Token.cs
@@ -0,0 +1,14 @@
+public class Token
+{
+    public int id { get; private set; }
+    public Guid externalId { get; private set; }
+    public string email { get; private set; }
+    public string refreshToken { get; private set; }
+    public DateTime createdAt { get; private set; }
+    public DateTime updatedAt { get; private set; }
+    public Token(){
+        externalId = Guid.NewGuid();
+        createdAt = DateTime.UtcNow;
+        updatedAt = DateTime.UtcNow;
+    }
+}
