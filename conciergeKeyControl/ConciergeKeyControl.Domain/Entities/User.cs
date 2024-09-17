c391b9f9e189170b4739da83c366866b18451fef
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 13:15:31 -0300

    feat: create the user entity

---
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+public class User{
+    public int id { get; private set; }
+    public Guid externalId {get; private set;}
+    public string name { get; private set;}
+    public string iv { get; private set;}
+    public DateTime createdAt { get; private set; }
+    public DateTime updatedAt { get; private set; }
+    public User(){
+        externalId = Guid.NewGuid();
+        createdAt = DateTime.UtcNow;
+        updatedAt = DateTime.UtcNow;
+    }
+}
