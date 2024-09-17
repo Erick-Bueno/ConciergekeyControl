19d67672cb32d64903e71f4bc6207ad6f73423c9
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 16:18:36 -0300

    feat: add key entity configuration

---
+++ conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/KeyEntityConfiguration.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class KeyEntityConfiguration : IEntityTypeConfiguration<Key>
+{
+    public void Configure(EntityTypeBuilder<Key> builder)
+    {
+        builder.HasKey(k => k.id);
+        builder.HasIndex(k => k.externalId);
+        builder.HasOne(k => k.room)
+        .WithMany(r => r.keys)
+        .HasForeignKey(k => k.idRoom);
+    }
+}
