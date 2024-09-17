cbe7aef133c2586febb6e3dca3e3a82ef36b3dec
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 14:51:47 -0300

    feat: add user entity configuration

---
+++ conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/UserEntityConfiguration.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<User>{
+    public void Configure(EntityTypeBuilder<User> builder){
+        builder.HasKey(u => u.id);
+        builder.HasIndex(u => u.externalId);
+        builder.Property(u => u.name).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.iv).IsRequired().HasMaxLength(300);
+
+    }
+}
