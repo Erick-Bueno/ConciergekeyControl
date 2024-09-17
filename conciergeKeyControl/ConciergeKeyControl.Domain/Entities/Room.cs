e2d268df91a4d593e60db9699ce6becb668c9310
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 13:20:53 -0300

    feat: create the room entity

---
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Room.cs
@@ -0,0 +1,14 @@
+public class Room
+{
+    public int id { get; private set; }
+    public Guid externalId { get; private set; }
+    public string name { get; private set; }
+    public DateTime createdAt { get; private set; }
+    public DateTime updatedAt { get; private set; }
+    public Room()
+    {
+        externalId = Guid.NewGuid();
+        createdAt = DateTime.UtcNow;
+        updatedAt = DateTime.UtcNow;
+    }
+}
