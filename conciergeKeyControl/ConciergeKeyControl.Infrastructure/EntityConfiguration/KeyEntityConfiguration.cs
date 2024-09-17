c4c5b31f828ecbd6e4262c84ffb0152153aefe29
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-17 16:20:29 -0300

    fix: update entity code structure

--- conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/KeyEntityConfiguration.cs
+++ conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/KeyEntityConfiguration.cs
@@ -2,10 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class KeyEntityConfiguration : IEntityTypeConfiguration<Key>
-{
-    public void Configure(EntityTypeBuilder<Key> builder)
-    {
+public class KeyEntityConfiguration : IEntityTypeConfiguration<Key>{
+    public void Configure(EntityTypeBuilder<Key> builder){
         builder.HasKey(k => k.id);
         builder.HasIndex(k => k.externalId);
         builder.HasOne(k => k.room)
