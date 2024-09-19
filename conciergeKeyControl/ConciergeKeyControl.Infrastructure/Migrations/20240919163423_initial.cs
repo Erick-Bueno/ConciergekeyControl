df181a1d118837218e78b8985e6a52ed42c2279f
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-19 13:43:48 -0300

    feat: add the migrations

---
+++ conciergeKeyControl/ConciergeKeyControl.Infrastructure/Migrations/20240919163423_initial.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConciergeKeyControl.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rooms",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    externalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rooms", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tokens",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    externalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    refreshToken = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tokens", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    externalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    iv = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "keys",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    externalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    idRoom = table.Column<int>(type: "int", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_keys", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_keys_rooms_idRoom",
+                        column: x => x.idRoom,
+                        principalTable: "rooms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reports",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    externalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    idUser = table.Column<int>(type: "int", nullable: false),
+                    idKey = table.Column<int>(type: "int", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    withdrawalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    returnDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reports", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_reports_keys_idKey",
+                        column: x => x.idKey,
+                        principalTable: "keys",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_reports_users_idUser",
+                        column: x => x.idUser,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_keys_externalId",
+                table: "keys",
+                column: "externalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_keys_idRoom",
+                table: "keys",
+                column: "idRoom");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reports_externalId",
+                table: "reports",
+                column: "externalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reports_idKey",
+                table: "reports",
+                column: "idKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reports_idUser",
+                table: "reports",
+                column: "idUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rooms_externalId",
+                table: "rooms",
+                column: "externalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tokens_externalId",
+                table: "tokens",
+                column: "externalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_externalId",
+                table: "users",
+                column: "externalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reports");
+
+            migrationBuilder.DropTable(
+                name: "tokens");
+
+            migrationBuilder.DropTable(
+                name: "keys");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "rooms");
+        }
+    }
+}
