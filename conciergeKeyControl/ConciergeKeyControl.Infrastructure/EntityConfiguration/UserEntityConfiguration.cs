e567c362026c4d49fdf0a13621bc336d7498557b
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-19 14:41:23 -0300

    feat: add email and password field in user entity configuration

--- conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/UserEntityConfiguration.cs
+++ conciergeKeyControl/ConciergeKeyControl.Infrastructure/EntityConfiguration/UserEntityConfiguration.cs
@@ -6,6 +6,8 @@ public class UserEntityConfiguration : IEntityTypeConfiguration<User>{
         builder.HasKey(u => u.id);
         builder.HasIndex(u => u.externalId);
         builder.Property(u => u.name).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.email).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.password).IsRequired().HasMaxLength(400);
         builder.Property(u => u.iv).IsRequired().HasMaxLength(300);
 
     }
