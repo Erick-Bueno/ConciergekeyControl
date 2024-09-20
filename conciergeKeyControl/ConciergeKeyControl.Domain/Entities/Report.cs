80b8780a851ff7fd0ba33b6c24dfad4fa0b6d82e
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-20 19:11:51 -0300

    refactor: change property names to PascalCase

--- conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Report.cs
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Report.cs
@@ -1,20 +1,20 @@
 public class Report
 {
-    public int id { get; private set; }
-    public Guid externalId { get; private set; }
-    public int idUser { get; private set; }
-    public int idKey { get; private set; }
-    public Status status { get; private set; }
-    public DateTime withdrawalDate { get; private set; }
-    public DateTime? returnDate { get; private set; }
-    public DateTime createdAt { get; private set; }
-    public DateTime updatedAt { get; private set; }
-    public Key key { get; private set; }
-    public User user { get; private set; }
+    public int Id { get; private set; }
+    public Guid ExternalId { get; private set; }
+    public int IdUser { get; private set; }
+    public int IdKey { get; private set; }
+    public Status Status { get; private set; }
+    public DateTime WithdrawalDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public Key Key { get; private set; }
+    public User User { get; private set; }
     public Report(){
-        externalId = Guid.NewGuid();
-        createdAt = DateTime.UtcNow;
-        updatedAt = DateTime.UtcNow;
-        status = Status.Available;
+        ExternalId = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+        Status = Status.Available;
     }
 }
