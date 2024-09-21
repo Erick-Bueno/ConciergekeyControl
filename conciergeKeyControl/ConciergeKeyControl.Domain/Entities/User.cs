8812a236927871d88d42e322deb1bc10253442ff
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-20 22:55:13 -0300

    refactor: change property and method  names to PascalCase

--- conciergeKeyControl/ConciergeKeyControl.Domain/Entities/User.cs
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/User.cs
@@ -1,32 +1,32 @@
 public class User{
-    public int id { get; private set; }
-    public Guid externalId {get; private set;}
-    public string name { get; private set;}
-    public string email { get; private set;}
-    public string password { get; private set;}
-    public string iv { get; private set;}
-    public DateTime createdAt { get; private set; }
-    public DateTime updatedAt { get; private set; }
-    public List<Report> reports { get; private set; }
+    public int Id { get; private set; }
+    public Guid ExternalId {get; private set;}
+    public string Name { get; private set;}
+    public string Email { get; private set;}
+    public string Password { get; private set;}
+    public string Iv { get; private set;}
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public List<Report> Reports { get; private set; }
     public User(){
-        externalId = Guid.NewGuid();
-        createdAt = DateTime.UtcNow;
-        updatedAt = DateTime.UtcNow;
+        ExternalId = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
     }
-    public User setName(string name){
-        this.name = name;
+    public User SetName(string name){
+        this.Name = name;
         return this;
     }
-    public User setEmail(string email){
-        this.email = email;
+    public User SetEmail(string email){
+        this.Email = email;
         return this;
     }
-    public User setPassword(string password){
-        this.password = password;
+    public User SetPassword(string password){
+        this.Password = password;
         return this;
     }
-    public User setIv(string iv){
-        this.iv = iv;
+    public User SetIv(string iv){
+        this.Iv = iv;
         return this;
     }
 }
