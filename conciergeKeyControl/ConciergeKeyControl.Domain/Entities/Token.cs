df4136703ce93e7f7a0c0503c1903cf4829049c8
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-20 22:53:42 -0300

    refactor: change property names to PascalCase

--- conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Token.cs
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Entities/Token.cs
@@ -1,13 +1,13 @@
 public class Token{
-    public int id { get; private set; }
-    public Guid externalId { get; private set; }
-    public string email { get; private set; }
-    public string refreshToken { get; private set; }
-    public DateTime createdAt { get; private set; }
-    public DateTime updatedAt { get; private set; }
+    public int Id { get; private set; }
+    public Guid ExternalId { get; private set; }
+    public string Email { get; private set; }
+    public string RefreshToken { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
     public Token(){
-        externalId = Guid.NewGuid();
-        createdAt = DateTime.UtcNow;
-        updatedAt = DateTime.UtcNow;
+        ExternalId = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
     }
 }
