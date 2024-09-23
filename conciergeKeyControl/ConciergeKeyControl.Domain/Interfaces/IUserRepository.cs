9fb4c1704185d5c1783e7f48e95184fa5f40566d
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-23 13:13:28 -0300

    fix: change the return of the create user method to no return

--- conciergeKeyControl/ConciergeKeyControl.Domain/Interfaces/IUserRepository.cs
+++ conciergeKeyControl/ConciergeKeyControl.Domain/Interfaces/IUserRepository.cs
@@ -2,5 +2,5 @@ using System.Diagnostics.Contracts;
 
 public interface IUserRepository{
     public User FindUserByEmail(string email);
-    public Task<User> CreateUser(User user);
+    public Task CreateUser(User user);
 }
