efe26f7cc37adccc3ed9269e5f96bed17bbb34be
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-23 13:14:42 -0300

    feat: add constructor in user registration response class

--- conciergeKeyControl/ConciergeKeyControl.Application/Responses/ResponseUserRegisterSuccess.cs
+++ conciergeKeyControl/ConciergeKeyControl.Application/Responses/ResponseUserRegisterSuccess.cs
@@ -1,6 +1,10 @@
-public class ResponseUserRegisterSuccess
-{
+public class ResponseUserRegisterSuccess{
     public int Status { get; set; }
     public string Message { get; set; }
+
+    public ResponseUserRegisterSuccess(int status, string message) {
+        Status = status;
+        Message = message;
+    }
     
 }
