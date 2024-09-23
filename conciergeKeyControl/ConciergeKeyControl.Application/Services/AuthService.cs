cf6ab1a575e5378251e826b750daf1176143f0fe
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2024-09-23 13:29:22 -0300

    feat: add auth method implementation

--- conciergeKeyControl/ConciergeKeyControl.Application/Services/AuthService.cs
+++ conciergeKeyControl/ConciergeKeyControl.Application/Services/AuthService.cs
@@ -1,7 +1,32 @@
 using OneOf;
-
+using Microsoft.Extensions.Configuration;
 public class AuthService : IAuhtService{
-    public Task<OneOf<Error, ResponseUserRegisterSuccess>> Register(UserRegisterDto userRegisterDto){
-        throw new NotImplementedException();
+    private readonly IUserRepository _UserRepository;
+    private readonly IConfiguration _Configuration;
+    private readonly IAes256 _Aes256;
+
+    public AuthService(IUserRepository userRepository, IAes256 aes256)
+    {
+        _UserRepository = userRepository;
+        _Aes256 = aes256;
+    }
+
+    public async Task<OneOf<Error, ResponseUserRegisterSuccess>> Register(UserRegisterDto userRegisterDto){
+        try
+        {
+            var user = _UserRepository.FindUserByEmail(userRegisterDto.Email);
+            if(user is not null){
+                return new EmailAlreadyRegistered("Email ja cadastrado");
+            }
+            var key = _Configuration.GetValue<string>("keyCrypt");
+            var encryptPassword = _Aes256.Encrypt(userRegisterDto.Password, key, out string iv );
+            var newUser = UserMapper.ToEntity(userRegisterDto, iv);
+            await _UserRepository.CreateUser(newUser);
+            return new ResponseUserRegisterSuccess(201, "Usuário cadastrado com sucesso");
+        }
+        catch (Exception ex)
+        {
+            return new InternalServerError(ex.Message);
+        }
     }
 }
