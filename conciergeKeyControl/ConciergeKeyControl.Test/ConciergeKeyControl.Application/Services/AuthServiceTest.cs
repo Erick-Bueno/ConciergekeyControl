7d0a0afb4ff3cdd1259907a8baa5d021171572c9
Erick bueno <erickjb93@gmail.com>
2024-10-14 11:11:22 -0300

    test(Authentication Services): add borgus in test layer

--- conciergeKeyControl/ConciergeKeyControl.Test/ConciergeKeyControl.Application/Services/AuthServiceTest.cs
+++ conciergeKeyControl/ConciergeKeyControl.Test/ConciergeKeyControl.Application/Services/AuthServiceTest.cs
@@ -1,27 +1,35 @@
+using Bogus;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using Xunit;
 
-public class AuthServiceTest{
+public class AuthServiceTest
+{
+    private readonly Faker _faker = new Faker();
     [Fact]
     public async void Register_EmailAlreadyRegistered_ReturnEmailAlreadyRegisteredError(){
+        //act
         var userRepositoryMock = new Mock<IUserRepository>();
         var configuration = new ConfigurationBuilder()
         .Build();
         var aes256Mock = new Mock<IAes256>();
-
+        var email = _faker.Person.Email;
         var user = new User()
-        .SetEmail("erickjb93@gmail.com");
-        var userRegisterDto = new UserRegisterDto(){
-            Email = "erickjb93@gmail.com"
-        };
+        .SetEmail(email);
+        var userRegisterDto = new Faker<UserRegisterDto>().StrictMode(true)
+            .RuleFor(x => x.Email, email)
+            .RuleFor(x => x.Password,
+                f => f.Internet.Password())
+            .RuleFor(x => x.Name, f => f.Name.FullName());
 
         userRepositoryMock.Setup(ur => ur.FindUserByEmail(user.Email)).Returns(user);
         
         var authService = new AuthService(userRepositoryMock.Object, aes256Mock.Object, configuration);
         var response = new EmailAlreadyRegistered("Email ja cadastrado");
+        //act
         var result = await authService.Register(userRegisterDto);
         Assert.Equal(response.TypeError, result.AsT1.TypeError);
+        //assert
         Assert.Equal(response.ErrorCodeName, result.AsT1.ErrorCodeName);
     }
     [Fact]
@@ -34,18 +42,20 @@ public class AuthServiceTest{
         .AddInMemoryCollection(appSettingsJson)
         .Build();
         var aes256Mock = new Mock<IAes256>();
-        
+        var email = _faker.Person.Email;
         var user = new User()
-        .SetEmail("erickjb93@gmail.com");
-        var userRegisterDto = new UserRegisterDto(){
-            Email = "erickjb93@gmail.com",
-            Password = "Sirlei231@"
-        };
-        var encryptPassword = "kkdddkkdd";
-        string iv = "teste";
+        .SetEmail(email);
+        var userRegisterDtoFaker = new Faker<UserRegisterDto>().StrictMode(true)
+            .RuleFor(x => x.Email, email)
+            .RuleFor(x => x.Password,
+                f => f.Internet.Password())
+            .RuleFor(x => x.Name, f => f.Name.FullName());
+        var userRegisterDto = userRegisterDtoFaker.Generate();
+        var encryptPassword = _faker.Random.Hash();
+        string iv = _faker.Random.String();
         var newUser = UserMapper.ToEntity(userRegisterDto, iv);
         userRepositoryMock.Setup(ur => ur.FindUserByEmail(user.Email)).Returns((User)null);
-        aes256Mock.Setup(ae => ae.Encrypt(userRegisterDto.Password,  appSettingsJson["keyCrypt"], out iv)).Returns(encryptPassword);
+        aes256Mock.Setup(ae => ae.Encrypt(userRegisterDto.Password,appSettingsJson["keyCrypt"], out iv)).Returns(encryptPassword);
         userRepositoryMock.Setup(ur => ur.CreateUser(newUser));
         var authService = new AuthService(userRepositoryMock.Object, aes256Mock.Object, configuration);
         var response = new ResponseUserRegisterSuccess(201, "Usuário cadastrado com sucesso");
